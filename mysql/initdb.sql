af3e2aa007e821eee14a6b4c015e0b66349ffd60
Bruno Oliveira <58921482+brunoliveiradev@users.noreply.github.com>
2024-05-18 12:46:00 -0300

    [FEATURE]: Created a initdb.sql to setup the MySQL when first running.

---
+++ mysql/initdb.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS orders;
+
+USE orders;
+
+CREATE TABLE IF NOT EXISTS orders
+(
+    id          VARCHAR(255)   NOT NULL,
+    price       DECIMAL(10, 2) NOT NULL,
+    tax         DECIMAL(10, 2) NOT NULL,
+    final_price DECIMAL(10, 2) NOT NULL,
+    PRIMARY KEY (id)
+);
