14c5bd78b6db5273691f8138031957d66d9acbd8
Bruno Oliveira <58921482+brunoliveiradev@users.noreply.github.com>
2024-05-26 19:05:09 -0300

    [FEATURE]: Order Listing useCase implemented in gRPC

--- internal/infra/grpc/service/order_service.go
+++ internal/infra/grpc/service/order_service.go
@@ -6,28 +6,35 @@ import (
 	"context"
 )
 
+var cacheKey = "orders"
+
 type OrderService struct {
 	pb.UnimplementedOrderServiceServer
-	CreateOrderUseCase usecase.CreateOrderUseCase
+	createOrderUseCase *usecase.OrderCreationUseCase
+	listOrdersUseCase  *usecase.OrderListingUseCase
 }
 
-func NewOrderService(createOrderUseCase usecase.CreateOrderUseCase) *OrderService {
+func NewOrderService(createOrderUseCase *usecase.OrderCreationUseCase, listOrdersUseCase *usecase.OrderListingUseCase) *OrderService {
 	return &OrderService{
-		CreateOrderUseCase: createOrderUseCase,
+		createOrderUseCase: createOrderUseCase,
+		listOrdersUseCase:  listOrdersUseCase,
 	}
 }
 
 func (s *OrderService) CreateOrder(ctx context.Context, req *pb.CreateOrderRequest) (*pb.CreateOrderResponse, error) {
-	input := usecase.OrderInputDTO{
+	dto := usecase.OrderInputDTO{
 		Name:  req.Name,
 		Price: float64(req.Price),
 		Tax:   float64(req.Tax),
 	}
 
-	output, err := s.CreateOrderUseCase.Execute(input)
+	output, err := s.createOrderUseCase.Execute(dto)
 	if err != nil {
 		return nil, err
 	}
+
+	// Clear cache to ensure consistency
+	s.listOrdersUseCase.Cache.Delete(cacheKey)
 
 	return &pb.CreateOrderResponse{
 		Id:         output.ID,
@@ -36,4 +43,24 @@ func (s *OrderService) CreateOrder(ctx context.Context, req *pb.CreateOrderReque
 		Tax:        float32(output.Tax),
 		FinalPrice: float32(output.FinalPrice),
 	}, nil
+}
+
+func (s *OrderService) ListOrders(ctx context.Context, req *pb.ListOrdersRequest) (*pb.ListOrdersResponse, error) {
+	output, err := s.listOrdersUseCase.Execute()
+	if err != nil {
+		return nil, err
+	}
+
+	var orders []*pb.Order
+	for _, order := range output.Orders {
+		orders = append(orders, &pb.Order{
+			Id:         order.ID,
+			Name:       order.Name,
+			Price:      float32(order.Price),
+			Tax:        float32(order.Tax),
+			FinalPrice: float32(order.FinalPrice),
+		})
+	}
+
+	return &pb.ListOrdersResponse{Orders: orders}, nil
 }
